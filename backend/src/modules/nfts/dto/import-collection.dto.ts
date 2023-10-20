9db86057acec1c6e6e5a38dd7212d86d3bfcad2e
Hoang Khanh Nguyen <hoang.nguyen2@opskins.com>
2023-10-20 14:33:38 +0700

    Clear source base

--- backend/src/modules/nfts/dto/import-collection.dto.ts
+++
@@ -1,3 +0,0 @@
-export class ImportCollectionDto {
-  collectionAddress: string;
-}
--- backend/src/modules/nfts/dto/sync-nft.dto.ts
+++
@@ -1,9 +0,0 @@
-export class SyncNftDto {
-  owner: string;
-  tokenId: number;
-  tokenURI: string;
-  collectionName: string;
-  collectionSymbol: string;
-  collectionAddress: string;
-  isAvailable: boolean;
-}
