e39b397cc21b47178178d4bb7160e7f1770c8c4f
Hoang Khanh Nguyen <hoang.nguyen2@opskins.com>
2023-10-24 12:16:41 +0700

    Updated default db name

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -7,7 +7,7 @@ export const DB_HOST = process.env.POSTGRES_DB_HOST || "localhost";
 export const DB_PORT = process.env.POSTGRES_DB_PORT
 	? parseInt(process.env.POSTGRES_DB_PORT)
 	: 5432;
-export const DB_DATABASE = process.env.POSTGRES_DB_DATABASE || "arthur-db";
+export const DB_DATABASE = process.env.POSTGRES_DB_DATABASE || "arthurdb";
 export const DB_USERNAME = process.env.POSTGRES_DB_USERNAME || "postgres";
 export const DB_PASSWORD = process.env.POSTGRES_DB_PASSWORD || "postgres";
 export const DB_MAX_CONNECTION = process.env.POSTGRES_DB_MAX_CONNECTION_POLL
