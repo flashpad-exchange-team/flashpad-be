f4f21f15b7b0971214c4ca8cea9c939e79a30d68
Hoang Khanh Nguyen <hoang.nguyen2@opskins.com>
2023-10-25 13:06:37 +0700

    Update cors config

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(
 	cors({
-		origin: ["http://localhost:3000", "https://arthur.exchange"],
+		origin: ["http://localhost:3000", "https://www.arthur.exchange"],
 		credentials: true,
 	})
 );
