03df49f2d359c84c8c13167b8aeb0cdbb09e849a
Hoang Khanh Nguyen <hoang.nguyen2@opskins.com>
2023-10-31 21:22:02 +0700

    getLpPairs: Fixed field name in response - `tvl24h` -> `vol24h` - `tvl` -> `volAllTime`

--- src/services/lpPair.service.ts
+++ src/services/lpPair.service.ts
@@ -5,12 +5,12 @@ import { instanceToPlain } from "class-transformer";
 export const getAllPairs = async (page: number, limit: number) => {
 	const { total, data: allPairsData } = await lpPairRepository.getAllPairs(page, limit);
 	const result = await Promise.all(allPairsData.map(async (pair) => {
-		const tvl = await txService.getTotalVolumeByLp(pair.address, false);
-		const tvl24h = await txService.getTotalVolumeByLp(pair.address, true);
+		const volAllTime = await txService.getTotalVolumeByLp(pair.address, false);
+		const vol24h = await txService.getTotalVolumeByLp(pair.address, true);
 		return {
 			...instanceToPlain(pair),
-			tvl,
-			tvl24h,
+			volAllTime,
+			vol24h,
 		}
 	}));
 	return {
@@ -22,12 +22,12 @@ export const getAllPairs = async (page: number, limit: number) => {
 export const getOnePair = async (lpAddress: string) => {
 	const pairData = await lpPairRepository.getPairByAddress(lpAddress);
 
-	const tvl = await txService.getTotalVolumeByLp(pairData.address, false);
-	const tvl24h = await txService.getTotalVolumeByLp(pairData.address, true);
+	const volAllTime = await txService.getTotalVolumeByLp(pairData.address, false);
+	const vol24h = await txService.getTotalVolumeByLp(pairData.address, true);
 
 	return {
 		...instanceToPlain(pairData),
-		tvl,
-		tvl24h,
+		volAllTime,
+		vol24h,
 	}
 };
