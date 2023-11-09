8a7a2115f7106e80b91f4b33adfb4f974c879861
htphong01 <thanhphonga4.hbt@gmail.com>
2023-11-09 22:35:49 +0700

    add api

--- src/controllers/merlinPool.controller.ts
+++ src/controllers/merlinPool.controller.ts
@@ -1,6 +1,9 @@
-import { ethers } from "ethers";
+import { ethers, JsonRpcProvider, Contract } from "ethers";
 import { Request, Response } from "express";
 import * as merlinPoolService from "../services/merlinPool.service";
+import { abi as MERLIN_POOL_ABI } from "../resources/MerlinPool.json";
+import { abi as NFT_POOL_ABI } from "../resources/NFTPool.json";
+import { RPC_URL } from "../configs/constants";
 
 export const getInfo = async (req: Request, res: Response) => {
   try {
@@ -18,9 +21,29 @@ export const getInfo = async (req: Request, res: Response) => {
       });
     }
 
+    const provider = new JsonRpcProvider(RPC_URL);
+    const merlinPoolContract = new Contract(address, MERLIN_POOL_ABI, provider);
+
+    const nftPoolAddress = await merlinPoolContract.nftPool();
+
+    const nftPoolContract = new Contract(nftPoolAddress, NFT_POOL_ABI, provider);
+
+    const [totalDepositAmount, rewardToken1, rewardToken2, nftPoolInfo] = await Promise.all([
+      merlinPoolContract.totalDepositAmount(),
+      merlinPoolContract.rewardsToken1(),
+      merlinPoolContract.rewardsToken2(),
+      nftPoolContract.getPoolInfo()
+    ]);
+
+    console.log('nftPoolInfo', nftPoolInfo[0])
+
     const response: any = {
       message: "ok",
       address,
+      totalDepositAmount: totalDepositAmount.toString(),
+      rewardToken1: rewardToken1.token,
+      rewardToken2: rewardToken2.token,
+      nftPoolAddress,
     };
 
     return res.status(200).json(response);
